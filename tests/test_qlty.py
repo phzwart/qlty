7303b5f7d199f012d3a8e5702fc2f0c8d1f3e775
phzwart <phzwart@lbl.gov>
2021-10-20 18:42:11 -0700

    first qlty commit with boilerplate code

---
+++ tests/test_qlty.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `qlty` package."""
+
+
+import unittest
+
+from qlty import qlty
+
+
+class TestQlty(unittest.TestCase):
+    """Tests for `qlty` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
