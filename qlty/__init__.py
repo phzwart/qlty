0b289b3ada034bdc00fe297223f1f508a3860d97
phzwart <PHZwart@lbl.gov>
2025-07-02 10:03:08 -0700

    Expose main classes and cleanup functions in package __init__.py
    - Import NCYXQuilt, NCZYXQuilt, LargeNCYXQuilt, LargeNCZYXQuilt classes
    - Import weed_sparse_classification_training_pairs_2D and 3D functions
    - Make all functionality available directly from qlty package
    - Fixes issue where users needed to import individual modules

--- qlty/__init__.py
+++ qlty/__init__.py
@@ -3,3 +3,22 @@
 __author__ = """Petrus H. Zwart"""
 __email__ = 'PHZwart@lbl.gov'
 __version__ = '0.2.0'
+
+# Import main classes from all modules
+from .qlty2D import NCYXQuilt
+from .qlty3D import NCZYXQuilt
+from .qlty2DLarge import LargeNCYXQuilt
+from .qlty3DLarge import LargeNCZYXQuilt
+
+# Import cleanup functions
+from .cleanup import weed_sparse_classification_training_pairs_2D, weed_sparse_classification_training_pairs_3D
+
+# Make all classes and functions available at the top level
+__all__ = [
+    'NCYXQuilt',
+    'NCZYXQuilt',
+    'LargeNCYXQuilt',
+    'LargeNCZYXQuilt',
+    'weed_sparse_classification_training_pairs_2D',
+    'weed_sparse_classification_training_pairs_3D'
+]
